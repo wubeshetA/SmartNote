48699dfd7711da88d0ca1bd4b08caf1a23574da7
freedisch <freeproduc@gmail.com>
2023-10-12 07:49:51 +0100

    added question page
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- lib/views/screens/question/questionsList.dart
+++ lib/views/screens/question/questionsList.dart
@@ -9,10 +9,42 @@ class Questions extends StatefulWidget {
 }
 
 class _QuestionsState extends State<Questions> {
+  List<Note> notes = [
+    Note(title: "Microbiology", date: DateTime.now()),
+    Note(title: "Science", date: DateTime.now()),
+    Note(title: "Algebra linear", date: DateTime.now()),
+    Note(title: "Calculus", date: DateTime.now()),
+    Note(title: "World Economic", date: DateTime.now()),
+    Note(title: "Arts Story", date: DateTime.now()),
+  ];
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text('Questions page'),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView Navigation'),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              final note = notes[index];
+              return Card(
+                child: ListTile(
+                  title: Text(note.title),
+                  subtitle: Text(note.date.toString()),
+                  trailing: const Icon(Icons.question_answer_outlined),
+                ),
+              );
+            }),
+      );
+}
+
+class Note {
+  final String title;
+  final DateTime date;
+
+  const Note({
+    required this.title,
+    required this.date,
+  });
 }
